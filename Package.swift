eeeb4a037965ab8dd1a6be349f0f20a2aec994d6
Benjamin Wen <ben-wen@outlook.com>
2023-12-07 21:24:48 +0800

    Remove dependencies and test target

--- Package.swift
+++ Package.swift
@@ -1,23 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "gitblog",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-    ],
+    platforms: [.macOS(.v14)],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "gitblog",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
-        .testTarget(
-            name: "gitblogTests",
-            dependencies: ["gitblog"]),
+            name: "gitblog"),
     ]
 )
