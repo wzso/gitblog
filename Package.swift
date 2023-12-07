feaca9876b1d58fbb6b1c29e6957756cf9a931b0
Benjamin Wen <ben-wen@outlook.com>
2023-12-07 23:15:34 +0800

    Fix github action issue

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "gitblog",
+    platforms: [.macOS(.v12)],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
