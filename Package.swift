68408e9145bfa72e95a934f415fa0957bee22fcf
Benjamin Wen <ben-wen@outlook.com>
2023-12-07 23:13:41 +0800

    Remove dependencies

--- Package.swift
+++ Package.swift
@@ -5,17 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "gitblog",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "gitblog",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
+            name: "gitblog"),
         .testTarget(
             name: "gitblogTests",
             dependencies: ["gitblog"]),
