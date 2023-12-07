00217e38c1098d1217f38322f20c3d4244a4470a
Benjamin Wen <ben-wen@outlook.com>
2023-12-07 22:31:48 +0800

    Fix swift-tools compatibility issue

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "gitblog",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v13)],
     targets: [
         .executableTarget(
             name: "gitblog"),
