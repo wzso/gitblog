05ebf925e1689c18fd0efe3ca85a86fb4612b1c0
Benjamin Wen <ben-wen@outlook.com>
2023-12-09 20:05:58 +0800

    Read env variables

--- Sources/gitblog/gitblog.swift
+++ Sources/gitblog/gitblog.swift
@@ -1,8 +1,17 @@
+import Foundation
+
 @main
 public struct gitblog {
-    public private(set) var text = "Hello, World!"
+    static let repository = ProcessInfo.processInfo.environment["REPOSITORY"]
+    static let githubToken = ProcessInfo.processInfo.environment["GITHUB_TOKEN"]
+    static let eventName = ProcessInfo.processInfo.environment["EVENT_NAME"]
+    static let issueNumber = ProcessInfo.processInfo.environment["ISSUE_NUMBER"]
 
     public static func main() {
-        print(gitblog().text)
+        print(eventName)
+        print(repository)
+        print(githubToken?.count)
+        print(eventName)
+        print(issueNumber)
     }
 }
